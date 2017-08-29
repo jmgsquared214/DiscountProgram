055c9c4982ebf23ff89917aad1b70f3acedc1dbf
jmgsquared214 <jmgsquared214@gmail.com>
2017-08-29 10:52:05 -0700

    added discount class and rules file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # DiscountProgram
+Rule conditions use a dialect called MVFLEX Expression Language (MVEL)
+when...then...end
+
+salience - a keyword sets priority to a rule
---
+++ src/main/java/com/tacoshop/Purchase.java
@@ -0,0 +1,44 @@
+package com.tacoshop;
+
+import java.math.BigDecimal;
+
+public class Purchase {
+	private BigDecimal total;
+	private int tacoCount;
+	private boolean drinkIncluded;
+	private double discount;
+	
+	public Purchase(BigDecimal total, int tacoCount, boolean drinkIncluded, double discount) {
+		this.total = total;
+		this.tacoCount = tacoCount;
+		this.drinkIncluded = drinkIncluded;
+		this.discount = 0;
+	}
+	
+	public BigDecimal getTotal() {
+		return total;
+	}
+	public void setTotal(BigDecimal total) {
+		this.total = total;
+	}
+	public int getTacoCount() {
+		return tacoCount;
+	}
+	public void setTacoCount(int tacoCount) {
+		this.tacoCount = tacoCount;
+	}
+	public boolean getDrinkIncluded() {
+		return drinkIncluded;
+	}
+	public void setDrinkIncluded(boolean drinkIncluded) {
+		this.drinkIncluded = drinkIncluded;
+	}
+	
+	public double getDiscount() {
+		return discount;
+	}
+	public void setDiscount(double discount) {
+		this.discount = discount;
+	}
+
+}
